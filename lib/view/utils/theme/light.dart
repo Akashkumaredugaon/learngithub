f1f0af9652b6c8f61330a08635a54a5987bdf928
Akashkumaredugaon <162021064+Akashkumaredugaon@users.noreply.github.com>
2024-07-03 09:05:06 +0530

    now i am change scaffold background colors

--- lib/view/utils/theme/light.dart
+++ lib/view/utils/theme/light.dart
@@ -5,6 +5,7 @@ import 'package:newsapp/view/utils/colors.dart';
 var lightThem=ThemeData(
 
     cardTheme: CardTheme(color: Colors.blueAccent),
+    // scaffoldBackgroundColor: Colors.black,
     scaffoldBackgroundColor: Colors.black,
     elevatedButtonTheme: ElevatedButtonThemeData(style: ButtonStyle( backgroundColor: MaterialStateProperty.all<Color>(Colors.lightBlueAccent.shade100))),
     appBarTheme: AppBarTheme(
@@ -18,7 +19,7 @@ var lightThem=ThemeData(
     textTheme: TextTheme(bodyText2: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 10)),
     iconTheme: IconThemeData(color: Colors.white),
 
-    listTileTheme:ListTileThemeData(titleTextStyle: TextStyle(color: Colors.white))
+
 
 
 
